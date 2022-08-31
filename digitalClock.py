55552d796ab2391aba009baf4cb63aa7edb2d2e5
dantechno1 <danneze03@gmail.com>
2022-08-31 15:01:54 -0400

    change bg to orange

--- digitalClock.py
+++ digitalClock.py
@@ -1,11 +1,18 @@
 from tkinter import Tk, Label
+from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="orange")
 
-label = Label(window, text="Welcome to DevOps Class!", font=("Arial Black",30,"bold"), bg="steelblue", fg="white")
+label = Label(window, text="Welcome to DevOps Class!", font=("Arial Black",30,"bold"), bg="orange", fg="white")
 label.pack(pady=100)
 
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
+
+clock()
 window.mainloop()
