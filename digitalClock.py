a0a9b4d8d65fd35dd01b63594cecbbf346187805
dantechno1 <danneze03@gmail.com>
2022-08-31 10:44:04 -0400

    change font value

--- digitalClock.py
+++ digitalClock.py
@@ -5,7 +5,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window, text="Welcome to DevOps!", font=("Arial Black",50,"bold"), bg="steelblue", fg="white")
+label = Label(window, text="Welcome to DevOps!", font=("Arial Black",30,"bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
 
 window.mainloop()
